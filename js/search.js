6787c685b8246ca59202b315d01c9d20f210cd00
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-01-06 17:50:45 +0530

    Delete js directory

--- js/search.js
+++
@@ -1,8 +0,0 @@
-function searchDocs(q) {
-  q = q.toLowerCase();
-  document.querySelectorAll(".doc").forEach(el => {
-    el.style.display = el.textContent.toLowerCase().includes(q)
-      ? "block"
-      : "none";
-  });
-}
